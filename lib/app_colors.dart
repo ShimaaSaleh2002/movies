b193ebba68f7a959188bcb72cb272ba8698af6c2
aya <ayakamal4545@gmail.com>
2024-08-30 16:38:25 +0300

    Initial commit

---
+++ lib/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static Color whiteColor= Color(0xffffffff);
+  static Color yellowColor= Color(0xffF2D352);
+  static Color greyColor= Color(0xff4F5A69);
+  static Color blackColor= Color(0xff303030);
+
+}
